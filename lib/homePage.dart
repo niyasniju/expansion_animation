fab5bc76c7495555e5a6768a3cca897d4b41b92e
muhammed niyas kc <nijukc889@mail.com>
2024-07-03 14:30:27 +0530

    initail

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -26,37 +27,86 @@ class MyExpansionTile extends StatelessWidget {
       children: <Widget>[
         Card(
           child: ExpansionTile(
-            leading: Icon(Icons.info),
-            title: Text('Expansion Tile 1'),
-            subtitle: Text('Tap to expand'),
+            leading: const Icon(Icons.info),
+            title: const Text('Expansion Tile 1'),
+            subtitle: const Text('Tap to expand'),
             children: <Widget>[
               Container(
                 color: Colors.lightGreenAccent,
-                child: ListTile(
-                  title: Text('Item 1'),
-                  subtitle: Text('Details of Item 1'),
+                child: Column(
+                  children: [
+                    const ListTile(
+                      title: Text('Item 1'),
+                      subtitle: Text('Details of Item 1'),
+                    ),
+                    Lottie.asset(
+                      'assets/polite.json',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    ),
+                  ],
                 ),
               ),
-              ListTile(
-                title: Text('Item 2'),
-                subtitle: Text('Details of Item 2'),
+              Container(
+                color: Colors.lightBlueAccent,
+                child: Column(
+                  children: [
+                    const ListTile(
+                      title: Text('Item 2'),
+                      subtitle: Text('Details of Item 2'),
+                    ),
+                    Lottie.asset(
+                      'assets/polite.json',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
         Card(
           child: ExpansionTile(
-            leading: Icon(Icons.info),
-            title: Text('Expansion Tile 2'),
-            subtitle: Text('Tap to expand'),
+            leading: const Icon(Icons.info),
+            title: const Text('Expansion Tile 2'),
+            subtitle: const Text('Tap to expand'),
             children: <Widget>[
-              ListTile(
-                title: Text('Item 3'),
-                subtitle: Text('Details of Item 3'),
+              Container(
+                color: Colors.lightBlueAccent,
+                child: Column(
+                  children: [
+                    const ListTile(
+                      title: Text('Item 3'),
+                      subtitle: Text('Details of Item 3'),
+                    ),
+                    Lottie.asset(
+                      'assets/polite.json',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    ),
+                  ],
+                ),
               ),
-              ListTile(
-                title: Text('Item 4'),
-                subtitle: Text('Details of Item 4'),
+              Container(
+                color: Colors.lightBlueAccent,
+                child: Column(
+                  children: [
+                    const ListTile(
+                      title: Text('Item 4'),
+                      subtitle: Text('Details of Item 4'),
+                    ),
+                    Lottie.asset(
+                      'assets/polite.json',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    )
+                  ],
+                ),
               ),
             ],
           ),
